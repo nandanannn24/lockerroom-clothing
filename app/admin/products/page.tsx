d46caa3551a01f758848b9f0db65116c501c0054
Priyoga Listyo Ananda <priyogalistyoananda26@gmail.com>
2026-04-15 21:00:43 +0700

    Update page.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { EditPriceForm } from "./EditPriceForm";
+export const dynamic = 'force-dynamic';
 
 export default async function ProductsPage() {
   const products = await prisma.product.findMany({
