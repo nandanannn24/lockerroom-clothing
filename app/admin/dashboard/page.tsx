f0ac906f64fd4de1af10cf0f0fab1864f99e5aee
Priyoga Listyo Ananda <priyogalistyoananda26@gmail.com>
2026-04-15 21:00:12 +0700

    Add dynamic export to AdminDashboardPage

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+export const dynamic = 'force-dynamic';
 
 export default async function AdminDashboardPage() {
   const orderCount = await prisma.order.count();
